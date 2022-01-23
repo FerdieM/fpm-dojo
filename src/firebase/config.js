870f101b1b88da6d42595af51a9b9d4808d3a90b
FerdieM <fm0707@yahoo.com>
2022-01-22 18:38:21 -0700

    Add Firestore Config and rules

---
+++ src/firebase/config.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAfjzHKU_AoHACuzJRSnVYGMRK1L2MaHEQ',
+  authDomain: 'dojo-site-b0c15.firebaseapp.com',
+  projectId: 'dojo-site-b0c15',
+  storageBucket: 'dojo-site-b0c15.appspot.com',
+  messagingSenderId: '966689023516',
+  appId: '1:966689023516:web:a6d89cc632d61f7404e1d9',
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp
+
+export { projectAuth, projectFirestore, timestamp};
