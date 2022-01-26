c00d3579e72a3f5121e797ca587e4e4718ac6d4d
FerdieM <fm0707@yahoo.com>
2022-01-26 14:55:45 -0700

    Adding Filter, rules and Final Touches on date

---
+++ src/pages/dashboard/ProjectFilter.js
@@ -0,0 +1,33 @@
+const filterlist = [
+  'all',
+  'mine',
+  'development',
+  'design',
+  'marketing',
+  'sales',
+];
+
+const ProjectFilter = ({ currentFilter, changeFilter }) => {
+  const handleClick = (newFilter) => {
+    changeFilter(newFilter);
+  };
+
+  return (
+    <div className="project-filter">
+      <nav>
+        <p>Filter by:</p>
+        {filterlist.map((f) => (
+          <button
+            key={f}
+            onClick={() => handleClick(f)}
+            className={currentFilter === f ? 'active' : ''}
+          >
+            {f}
+          </button>
+        ))}
+      </nav>
+    </div>
+  );
+};
+
+export default ProjectFilter;
